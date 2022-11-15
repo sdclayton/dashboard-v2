faca13e20ffcf56811a94081fda1d4eae183a81f
sdclayton <96846005+sdclayton@users.noreply.github.com>
2022-11-14 20:21:58 -0600

    added login & signup, need to finish formGroup in signup...signup not show

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  // property to hold form
+  public signupForm: FormGroup;
 
-  constructor() { }
+  // inject FormBuilder
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    // initalize the from control
+    this.signupForm = this.formBuilder.group({
+      fullName: [''],
+      phoneNumber: [''],
+      emailAddress: [''],
+      password: ['']
+    })
+  }
+
+  signUp() {
+
   }
 
 }
